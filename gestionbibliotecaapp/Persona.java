cc1b3a5adcf1cd14439783bccf4590bf9cf63b34
AlvaroAlme <3888170@alu.murciaeduca.es>
2025-05-18 13:05:15 +0200

    ACtualiza commit

---
+++ gestionbibliotecaapp/Persona.java
@@ -0,0 +1,54 @@
+import java.util.Date;
+
+public class Persona{
+
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private int fechaNacimiento;
+
+    public Persona(){
+        super();
+    }
+
+    public Persona(String nombre, String apellido, String dni, int fechaNacimiento){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(int fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    
+}
